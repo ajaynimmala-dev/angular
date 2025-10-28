27f5c74224c807a22b718d7b72aa12cf3b0bee60
Ajay <ajaynimmala18@gmail.com>
2025-10-28 12:23:45 +0530

    for,if,else control statements and added a section to display the tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor,NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import {UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -9,7 +10,7 @@ import { TasksComponent } from './tasks/tasks.component';
   {
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent,TasksComponent],
+  imports: [HeaderComponent,UserComponent,TasksComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   }
@@ -19,7 +20,7 @@ import { TasksComponent } from './tasks/tasks.component';
 export class AppComponent {
 
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?:string;
 
   get selectedUser(){
     return this.users.find((user)=>user.id == this.selectedUserId);
