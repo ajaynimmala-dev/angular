27dad98235f7616268f6cdcc39a2856cffb9c1ea
Ajay <ajaynimmala18@gmail.com>
2025-11-01 15:10:52 +0530

    remove Standalone property and Created Module using NgModule

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component'
+import {TaskComponent} from "./tasks/task/task.component";
+import {NewTaskComponent} from "./tasks/new-task/new-task.component";
+import {SharedModule} from "./shared/shared.module";
+
+@NgModule({
+  declarations: [AppComponent,HeaderComponent,UserComponent,TasksComponent,TaskComponent,NewTaskComponent],
+  bootstrap:[AppComponent],
+  imports :[ BrowserModule,FormsModule,SharedModule]
+})
+export class AppModule {}
